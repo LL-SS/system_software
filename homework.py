e3a405ae5345d79674b54e42778546f39c41a890
ll-ss <leesw0529@gmail.com>
2022-04-30 05:26:59 +0900

    homework modified

--- homework.py
+++ homework.py
@@ -1,25 +1,45 @@
 import hashlib
-import string
-import random
-
-base_str = 'homework'
-string_pool = string.ascii_letters
-str_x = ''
+import time
 
 # find a collision of SHA-256
-length = 1
+def find_collision(zero_length):
+    concat = 0
+    start = time.time()
 
-# while True:
-for i in string_pool:
-    str_x = i
+    while True:
+        enc_str = hashlib.sha256(('homework' + str(concat)).encode()).hexdigest()[:5]
+        bin_bit = bin(int(enc_str, 16))[2:].zfill(20)
 
-enc_str = int(hashlib.sha256((base_str + str_x).encode()).hexdigest(), 16)
-print(format(enc_str, 'b')[:2])
-# print(format(enc_str, 'b')[:2], str_x, sep='\n')
+        if int(bin_bit[:zero_length], 10) == 0:
+            end = time.time()
+            print('binary output:', bin_bit)
+            print('nonce x:', concat)
+            print('execution time:', end - start)
+            break
 
-    # if int(format(enc_str, 'b')[0]) == 0:
-    #     break
+        concat = int(concat) + 1
 
+def find_collision_double(zero_length):
+    concat = 0
+    start = time.time()
 
+    while True:
+        enc_str = hashlib.sha256(('homework' + str(concat)).encode()).digest()
+        enc_str_double = hashlib.sha256(enc_str).hexdigest()[:5]
+        bin_bit = bin(int(enc_str_double, 16))[2:].zfill(20)
 
-# print(str_x)
+        if int(bin_bit[:zero_length], 10) == 0:
+            end = time.time()
+            print('binary output:', bin_bit)
+            print('nonce x:', concat)
+            print('execution time:', end - start)
+            break
+
+        concat = int(concat) + 1
+
+# Increase the number of 0’s from 1 to 20 bits and plots the execution time
+for i in range(1, 21):
+    find_collision(i)
+
+for i in range(1, 21):
+    find_collision_double(i)
